3f66804d47e54c7a90ffbc50901e190ea49551fd
borkro <kronow.bo@gmail.com>
2020-12-07 14:55:15 +0100

    Math.round & data tooltips

--- Game/script.js
+++ Game/script.js
@@ -12,93 +12,93 @@ let simulation = null;
 
 
 function addDataDisplay(name, chartDataset, maxDays) {
-    dataDisplays.push({
-        name: name,
-        chartDataset: chartDataset,
-        maxDays: maxDays
-    });
+	dataDisplays.push({
+		name: name,
+		chartDataset: chartDataset,
+		maxDays: maxDays
+	});
 }
 
 function displayData(simDay) {
-    // Display all new data
-    dataDisplays.forEach(display => {
-        let chartData = display.chartDataset.data;
-        chartData.push(simDay[display.name]);
-        if (chartData.length > display.maxDays) {
-            chartData.shift();
-        }
-    });
+	// Display all new data
+	dataDisplays.forEach(display => {
+		let chartData = display.chartDataset.data;
+		chartData.push(simDay[display.name]);
+		if (chartData.length > display.maxDays) {
+			chartData.shift();
+		}
+	});
 
-    // Update all charts
-    charts.forEach(chart => {
-        let data = chart.data;
-	    data.labels.push(simDay.date);
-        if (data.labels.length > data.datasets[0].data.length) {
-            data.labels.shift();
-        }
-	    chart.update();
-    });
+	// Update all charts
+	charts.forEach(chart => {
+		let data = chart.data;
+		data.labels.push(simDay.date);
+		if (data.labels.length > data.datasets[0].data.length) {
+			data.labels.shift();
+		}
+		chart.update();
+	});
 
 	document.getElementById("datum").innerHTML = simDay.date;
 	document.getElementById("deadTotal").innerHTML = Math.round(simDay.deadTotal);
 	document.getElementById("deathsToday").innerHTML = Math.round(simDay.deathsToday);
-	document.getElementById("costTotal").innerHTML = simDay.costTotal/1e3;
+	document.getElementById("costTotal").innerHTML = simDay.costTotal / 1e3;
 }
 
 function copyWithDefault(dict, defaults) {
-    // Hacky way to do clone
-    let ret = JSON.parse(JSON.stringify(defaults));
-    for (var key in dict) {
-        ret[key] = dict[key];
-    }
+	// Hacky way to do clone
+	let ret = JSON.parse(JSON.stringify(defaults));
+	for (var key in dict) {
+		ret[key] = dict[key];
+	}
 
-    return ret;
+	return ret;
 }
 
 function createChart(canvasId, maxDays, datasets, yAxes) {
-    let canvas = document.getElementById(canvasId);
-    let canvasCtx = canvas.getContext("2d");
+	let canvas = document.getElementById(canvasId);
+	let canvasCtx = canvas.getContext("2d");
 
-    let datasetDefault = {
-			data: [],
-			borderWidth: 2,
-			lineTension: 0.4
+	let datasetDefault = {
+		data: [],
+		borderWidth: 2,
+		lineTension: 0.4
 	};
-    let colorDefault = [
-	    'rgba(  0,   0, 255, .7)',
-	    'rgba(255,   0,   0, .7)',
-	    'rgba(  0, 255,   0, .7)',
-    ];
+	let colorDefault = [
+		'rgba(  0,   0, 255, .7)',
+		'rgba(255,   0,   0, .7)',
+		'rgba(  0, 255,   0, .7)',
+	];
 
-    let chartDatasets = [];
+	let chartDatasets = [];
 
-    for(let i = 0; i < datasets.length; i++) {
-        let dataset = datasets[i];
-        let d = copyWithDefault(dataset, datasetDefault);
-        delete d.dataset;
-        if (!d["borderColor"]) {
-            d["borderColor"] = colorDefault[i];
-        }
-        chartDatasets.push(d);
-    }
+	for (let i = 0; i < datasets.length; i++) {
+		let dataset = datasets[i];
+		let d = copyWithDefault(dataset, datasetDefault);
+		delete d.dataset;
+		if (!d["borderColor"]) {
+			d["borderColor"] = colorDefault[i];
+		}
+		chartDatasets.push(d);
+	}
 
-    let chartYAxes = [];
-    let yAxisDefault = {
+	let chartYAxes = [];
+	let yAxisDefault = {
 		ticks: {
 			beginAtZero: true,
 		}
-    };
+	};
 
-    yAxes.forEach(yAxis => {
-        chartYAxes.push(copyWithDefault(yAxis, yAxisDefault));
-    });
+	yAxes.forEach(yAxis => {
+		chartYAxes.push(copyWithDefault(yAxis, yAxisDefault));
+	});
 	let chart = new Chart(canvasCtx, {
 		type: 'line',
 		fill: 'start',
 		data: {
-		    labels: [],
-		    datasets: chartDatasets
-        },
+			labels: [],
+			datasets: chartDatasets
+		},
 		options: {
 			layout: {
 				padding: {
@@ -116,93 +116,100 @@ function createChart(canvasId, maxDays, datasets, yAxes) {
 						maxRotation: 0 */
 					}
 				}]
+			},
+			tooltips: {
+				callbacks: {
+					label: function (tooltipItem) {
+						return Number(tooltipItem.yLabel);
+					}
+				}
 			}
 		}
 	});
 
-    for (let i=0; i < datasets.length; i++) {
-        addDataDisplay(datasets[i].dataset, chart.data.datasets[i], maxDays);
-    }
-    charts.push(chart);
+	for (let i = 0; i < datasets.length; i++) {
+		addDataDisplay(datasets[i].dataset, chart.data.datasets[i], maxDays);
+	}
+	charts.push(chart);
 }
 
 function formatWithThousandsSeparator(value, dec) {
-    if (value < 0) {
-        return "-" + formatWithThousandsSeparator(-value, dec);
-    }
+	if (value < 0) {
+		return "-" + formatWithThousandsSeparator(-value, dec);
+	}
 
-    let v = Math.floor(value);
-    let ret = "0";
-    // whole number part
-    if (v < 1000) {
-        ret = v.toString();
-    } else {
-        let a = (v % 1000 + 1000).toString().slice(1);
-        ret = formatWithThousandsSeparator(v / 1000, 0) + "," + a;
-    }
+	let v = Math.floor(value);
+	let ret = "0";
+	// whole number part
+	if (v < 1000) {
+		ret = v.toString();
+	} else {
+		let a = (v % 1000 + 1000).toString().slice(1);
+		ret = formatWithThousandsSeparator(v / 1000, 0) + "," + a;
+	}
 
-    if (dec > 0) {
-        let frac = Math.floor((1 + value - v) * Math.pow(10, dec)).toString().slice(1);
-        ret = ret + "." + frac;
-    }
+	if (dec > 0) {
+		let frac = Math.floor((1 + value - v) * Math.pow(10, dec)).toString().slice(1);
+		ret = ret + "." + frac;
+	}
 
-    return ret;
+	return ret;
 }
 
 function initialize() {
-    simulation = new CovidSimulation("2020-03-01");
+	simulation = new CovidSimulation("2020-03-01");
 
 	Chart.defaults.global.elements.point.backgroundColor = 'rgba(255, 99, 132, 0.3)';
 	Chart.defaults.global.elements.point.borderColor = 'rgba(255, 99, 132, .7)';
 	Chart.defaults.global.elements.point.borderWidth = 1;
 
-    var thousandsTicks = {
-			beginAtZero: true,
-            callback: function(value, index, values) {
-                return formatWithThousandsSeparator(value/1000, values[0] > 1000 ? 1 : 3);
-            }
-        };
-    var simpleLeftAxe = [ {id: 'left'} ];
-    var thousandsLeftAxe = [ {id: 'left', ticks: thousandsTicks} ];
-    var thousandsLeftRightAxes = [ {id: 'left', ticks: thousandsTicks},
-                           {id: 'right', ticks: thousandsTicks, position: 'right'} ];
+	var thousandsTicks = {
+		beginAtZero: true,
+		callback: function (value, index, values) {
+			return formatWithThousandsSeparator(value / 1000, values[0] > 1000 ? 1 : 3);
+		}
+	};
+	var simpleLeftAxe = [{ id: 'left' }];
+	var thousandsLeftAxe = [{ id: 'left', ticks: thousandsTicks }];
+	var thousandsLeftRightAxes = [{ id: 'left', ticks: thousandsTicks },
+	{ id: 'right', ticks: thousandsTicks, position: 'right' }];
 
-    var datasets11 = [ {
-			label: 'nově nakažených [tis]',
-            dataset: 'detectedInfectionsToday',
-            yAxisID: 'left'
-		}, {
-			label: 'nových mrtvých [tis]',
-            dataset: 'deathsToday',
-            yAxisID: 'right',
-		}];
-    createChart("chart-1-1", DISPLAY_N_DAYS, datasets11, thousandsLeftRightAxes);
+	var datasets11 = [{
+		label: 'nově nakažených [tis]',
+		dataset: 'detectedInfectionsToday',
+		yAxisID: 'left'
+	}, {
+		label: 'nových mrtvých [tis]',
+		dataset: 'deathsToday',
+		yAxisID: 'right',
+	}];
+	createChart("chart-1-1", DISPLAY_N_DAYS, datasets11, thousandsLeftRightAxes);
 
-    var datasets21 = [ {
-			label: 'aktuálně nakažených [tis]',
-            dataset: 'detectedActiveInfectionsTotal',
-            yAxisID: 'left'
-		}];
-    createChart("chart-2-1", DISPLAY_N_DAYS, datasets21, thousandsLeftAxe);
+	var datasets21 = [{
+		label: 'aktuálně nakažených [tis]',
+		dataset: 'detectedActiveInfectionsTotal',
+		yAxisID: 'left'
+	}];
+	createChart("chart-2-1", DISPLAY_N_DAYS, datasets21, thousandsLeftAxe);
 
-    var datasets22 = [ {
-			label: 'nakažených [tis]',
-            dataset: 'detectedInfectionsTotal',
-            yAxisID: 'left'
-		}, {
-			label: 'mrtvých [tis]',
-            dataset: 'deadTotal',
-            yAxisID: 'right',
-		}];
-    createChart("chart-2-2", DISPLAY_N_DAYS, datasets22, thousandsLeftRightAxes);
+	var datasets22 = [{
+		label: 'nakažených [tis]',
+		dataset: 'detectedInfectionsTotal',
+		yAxisID: 'left'
+	}, {
+		label: 'mrtvých [tis]',
+		dataset: 'deadTotal',
+		yAxisID: 'right',
+	}];
+	createChart("chart-2-2", DISPLAY_N_DAYS, datasets22, thousandsLeftRightAxes);
 
-     var datasets23 = [ {
-			label: 'smrtnost [%]',
-            dataset: 'mortalityPct',
-		}];
-    createChart("chart-2-3", DISPLAY_N_DAYS, datasets23, simpleLeftAxe);
+	var datasets23 = [{
+		label: 'smrtnost [%]',
+		dataset: 'mortalityPct',
+	}];
+	createChart("chart-2-3", DISPLAY_N_DAYS, datasets23, simpleLeftAxe);
 
-    simulation.simDayStats.forEach(day => displayData(day));
+	simulation.simDayStats.forEach(day => displayData(day));
 }
 initialize();
 
--- Game/simulation.js
+++ Game/simulation.js
@@ -1,137 +1,137 @@
 // Discrete SIR model variant with delay and reinfections
 
 class CovidSimulation {
-    constructor(startDate) {
-        // pandemic params
-        this.R0 = 2.5;
-        this.mortality = 0.012;
-        this.initialPopulation = 10690000;
-        this.infectedStart = 3;
+	constructor(startDate) {
+		// pandemic params
+		this.R0 = 2.5;
+		this.mortality = 0.012;
+		this.initialPopulation = 10690000;
+		this.infectedStart = 3;
 
-        // All covid parameters counted from the infection day
-        this.incubationDays = 5; // Days until infection is detected
-        this.infectiousFrom = 3; // First day when people are infectious
-        this.infectiousTo = 8;   // Last day when people are infectious (they will isolate after the onset of COVID)
-        this.recoveryDays = 14 + this.incubationDays;
-        this.timeToDeathDays = 21 + this.incubationDays;
-        this.immunityDays = 90 + this.recoveryDays;
+		// All covid parameters counted from the infection day
+		this.incubationDays = 5; // Days until infection is detected
+		this.infectiousFrom = 3; // First day when people are infectious
+		this.infectiousTo = 8;   // Last day when people are infectious (they will isolate after the onset of COVID)
+		this.recoveryDays = 14 + this.incubationDays;
+		this.timeToDeathDays = 21 + this.incubationDays;
+		this.immunityDays = 90 + this.recoveryDays;
 
-        this.simDays = [];
-        this.simDayStats = [];
+		this.simDays = [];
+		this.simDayStats = [];
 
-        this.simDays.push({
-            date: startDate,
-            suspectible: this.initialPopulation - this.infectedStart,
-            infected: this.infectedStart,
-            recovered: 0,
-            dead: 0,
-            infectedToday: this.infectedStart,
-            deathsToday: 0,
-            costToday: 0,
-        });
+		this.simDays.push({
+			date: startDate,
+			suspectible: this.initialPopulation - this.infectedStart,
+			infected: this.infectedStart,
+			recovered: 0,
+			dead: 0,
+			infectedToday: this.infectedStart,
+			deathsToday: 0,
+			costToday: 0,
+		});
 
-        this.calcStats();
-    }
+		this.calcStats();
+	}
 
-    getDayInPast(n) {
-        let i = this.simDays.length - n;
-        if (i >= 0) {
-            return this.simDays[i];
-        } else {
-            // Days before the start of the epidemic have no sick people
-            return {
-                date: null,
-                suspectible: this.initialPopulation,
-                infected: 0,
-                recovered: 0,
-                dead: 0,
-                infectedToday: 0,
-                deathsToday: 0,
-                costToday: 0,
-            };
-        }
-    }
+	getDayInPast(n) {
+		let i = this.simDays.length - n;
+		if (i >= 0) {
+			return this.simDays[i];
+		} else {
+			// Days before the start of the epidemic have no sick people
+			return {
+				date: null,
+				suspectible: this.initialPopulation,
+				infected: 0,
+				recovered: 0,
+				dead: 0,
+				infectedToday: 0,
+				deathsToday: 0,
+				costToday: 0,
+			};
+		}
+	}
 
-    simOneDay() {
-        let yesterday = this.getDayInPast(1);
-        let mRate = this.mortality;       // mortality rate
-        let sRate = 1. - this.mortality;  // survival rate
+	simOneDay() {
+		let yesterday = this.getDayInPast(1);
+		let mRate = this.mortality;       // mortality rate
+		let sRate = 1. - this.mortality;  // survival rate
 
-        let suspectible = yesterday.suspectible;
-        let infected = yesterday.infected;
-        let recovered = yesterday.recovered;
-        let dead = yesterday.dead;
+		let suspectible = yesterday.suspectible;
+		let infected = yesterday.infected;
+		let recovered = yesterday.recovered;
+		let dead = yesterday.dead;
 
-        let mitigation = getMitigation();
-        let R = this.R0 * mitigation.mult;
+		let mitigation = getMitigation();
+		let R = this.R0 * mitigation.mult;
 
-        let population = yesterday.suspectible + yesterday.infected + yesterday.recovered;
-        let infectious = 0.;
-        for (let i = this.infectiousFrom; i <= this.infectiousTo; ++i) {
-            infectious += this.getDayInPast(i).infectedToday;
-        }
-        infectious /= (this.infectiousTo - this.infectiousFrom + 1);
-        let infectedToday = infectious * R * yesterday.suspectible / population;
-        infected += infectedToday;
-        suspectible -= infectedToday;
+		let population = yesterday.suspectible + yesterday.infected + yesterday.recovered;
+		let infectious = 0.;
+		for (let i = this.infectiousFrom; i <= this.infectiousTo; ++i) {
+			infectious += this.getDayInPast(i).infectedToday;
+		}
+		infectious /= (this.infectiousTo - this.infectiousFrom + 1);
+		let infectedToday = Math.round(infectious * R * yesterday.suspectible / population);
+		infected += infectedToday;
+		suspectible -= infectedToday;
 
-        let recoveredToday = this.getDayInPast(this.recoveryDays).infectedToday * sRate;
-        recovered += recoveredToday;
-        infected -= recoveredToday;
+		let recoveredToday = Math.round(this.getDayInPast(this.recoveryDays).infectedToday * sRate);
+		recovered += recoveredToday;
+		infected -= recoveredToday;
 
-        let deathsToday = this.getDayInPast(this.timeToDeathDays).infectedToday * mRate;
-        dead += deathsToday;
-        infected -= deathsToday;
+		let deathsToday = Math.round(this.getDayInPast(this.timeToDeathDays).infectedToday * mRate);
+		dead += deathsToday;
+		infected -= deathsToday;
 
-        let endedImmunityToday = this.getDayInPast(this.immunityDays).infectedToday * sRate;
-        suspectible += endedImmunityToday;
-        recovered -= endedImmunityToday;
+		let endedImmunityToday = Math.round(this.getDayInPast(this.immunityDays).infectedToday * sRate);
+		suspectible += endedImmunityToday;
+		recovered -= endedImmunityToday;
 
-        this.simDays.push({
-            date: plusDay(yesterday.date),
-            suspectible: suspectible,
-            infected: infected,
-            recovered: recovered,
-            dead: dead,
-            infectedToday: infectedToday,
-            deathsToday: deathsToday,
-            costToday: mitigation.cost,
-            R: R,
-        });
+		this.simDays.push({
+			date: plusDay(yesterday.date),
+			suspectible: suspectible,
+			infected: infected,
+			recovered: recovered,
+			dead: dead,
+			infectedToday: infectedToday,
+			deathsToday: deathsToday,
+			costToday: mitigation.cost,
+			R: R,
+		});
 
-        return this.calcStats();
-    }
+		return this.calcStats();
+	}
 
-    calcStats() {
-        let today = this.getDayInPast(1);
-        let lastStat = (this.simDayStats.length > 0) ? this.simDayStats[this.simDayStats.length - 1] : null;
+	calcStats() {
+		let today = this.getDayInPast(1);
+		let lastStat = (this.simDayStats.length > 0) ? this.simDayStats[this.simDayStats.length - 1] : null;
 
 
-        let undetectedInfections = 0;
-        for(let i = 1; i <= this.incubationDays; i++) {
-            undetectedInfections += this.getDayInPast(i).infectedToday;
-        }
+		let undetectedInfections = 0;
+		for (let i = 1; i <= this.incubationDays; i++) {
+			undetectedInfections += this.getDayInPast(i).infectedToday;
+		}
 
-        let detectedInfectionsToday = this.getDayInPast(this.incubationDays + 1).infectedToday;
-        let detectedInfectionsTotal = ((lastStat != null) ? lastStat.detectedInfectionsTotal : 0)
-                + detectedInfectionsToday;
+		let detectedInfectionsToday = this.getDayInPast(this.incubationDays + 1).infectedToday;
+		let detectedInfectionsTotal = ((lastStat != null) ? lastStat.detectedInfectionsTotal : 0)
+			+ detectedInfectionsToday;
 
-        let costTotal = ((lastStat != null) ? lastStat.costTotal : 0) + today.costToday;
+		let costTotal = ((lastStat != null) ? lastStat.costTotal : 0) + today.costToday;
 
-        let stats = {
-            date: today.date,
-            deadTotal: today.dead,
-            deathsToday: today.deathsToday,
-            detectedInfectionsToday: detectedInfectionsToday,
-            detectedInfectionsTotal: detectedInfectionsTotal,
-            detectedActiveInfectionsTotal: today.infected - undetectedInfections,
-            mortalityPct: 100. * today.dead / detectedInfectionsTotal,
-            costTotal: costTotal,
-        };
+		let stats = {
+			date: today.date,
+			deadTotal: today.dead,
+			deathsToday: today.deathsToday,
+			detectedInfectionsToday: detectedInfectionsToday,
+			detectedInfectionsTotal: detectedInfectionsTotal,
+			detectedActiveInfectionsTotal: today.infected - undetectedInfections,
+			mortalityPct: 100. * today.dead / detectedInfectionsTotal,
+			costTotal: costTotal,
+		};
 
-        this.simDayStats.push(stats);
+		this.simDayStats.push(stats);
 
-        return stats;
-    }
- }
+		return stats;
+	}
+}
 
